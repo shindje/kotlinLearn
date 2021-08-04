5d9e5edada79fbbcfdfa007a77a2dcef7b72265b
shindje <aleks.shindje@gmail.com>
2021-08-04 14:49:58 +0300

    ProducerConsumer - Coroutines GlobalScope

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,3 +1,5 @@
+import kotlinx.coroutines.GlobalScope   //Нужно подключить либу kotlinx-coroutines-core
+import kotlinx.coroutines.launch
 import java.util.*
 import java.util.concurrent.Executors
 
@@ -5,49 +7,40 @@ fun main() {
     val pool = Executors.newFixedThreadPool(4)
     pool.submit({println("dfdf")})
     val pc = ProducerConsumer()
-    val t0 = Thread() {
+    GlobalScope.launch {
         while (true)
             pc.produce(2)
+
     }
-    val t1 = Thread() {
+    GlobalScope.launch {
         while (true)
             pc.produce(1)
+
     }
-    val t2 = Thread() {
+    GlobalScope.launch {
         while (true)
             pc.consume()
     }
-    t0.start()
-    t1.start()
-    t2.start()
-    t0.join()
-    t1.join()
-    t2.join()
 }
 
-//Java like
 class ProducerConsumer() {
     private val MAX_SIZE = 1000
     private val list = LinkedList<Int>()
 
     @Synchronized fun produce(n: Int){
-        //synchronized(this) {          // oldstyle
             if (list.size < MAX_SIZE) {
                 list.add(n*10_000 + (Math.random() * 1000).toInt())
                 (this as Object).notify()
             } else
                 (this as Object).wait()
-        //}
     }
 
     @Synchronized fun consume() {
-        //synchronized(this) {
             if (list.size > 0) {
                 println(list[0])
                 list.remove()
                 (this as Object).notify()
             } else
                 (this as Object).wait()
-        //}
     }
 }
